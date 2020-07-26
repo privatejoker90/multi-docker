6f616bcf148dd1e7aa1279da32f1043b5ba33189
Abhishek Khattri <khattr@adobe.com>
2020-07-26 20:37:25 +0530

    travis yml

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+export default () => {
+    return (
+        <div>
+            I am some other Page!
+            <Link to="/">Go back home</Link>
+        </div>
+    );
+}
